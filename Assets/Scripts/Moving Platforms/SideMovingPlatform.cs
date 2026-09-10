9bfdb60bed7645c0c477425b82aa928356ce3e6d
Santino <agatiello.santino@gmail.com>
2026-09-10 20:53:18 -0300

    Cambios a plataforma movil
    - Implementacion del enum "axis" para determinar la direccion de manera mas legible
    - Arreglado lo del spawnpoint que no funcionaba
    - Cambios en la manera en la que funciona SideMovingPlatform

--- Assets/Scripts/Moving Platforms/SideMovingPlatform.cs
+++ Assets/Scripts/Moving Platforms/SideMovingPlatform.cs
@@ -3,26 +3,51 @@ using UnityEngine.InputSystem.Controls;
 
 public class SideMovingPlatform : MovingPlatform
 {
+    //Distancia en la que se mueven las plataformas
     [SerializeField] private int distance;
-    [SerializeField] int axis;
-    private Vector3 movement = Vector3.zero;
-    void Start()
+    //Eje en el cual se mueven
+    [SerializeField] private AXIS axis;
+    //Direccion en la que se mueve
+    private int direction = 1;
+    protected override void Start()
     {
-        movement[axis] = 1;
-        movement*= speed * Time.deltaTime;
+        //Llama al start de la clase abstracta
+        base.Start();
+        //Establece la distancia a positivo, por las dudas
+        distance = Mathf.Abs(distance);
     }
 
     void Update()
     {
+       //Verifica si tiene que cambiar la direccion
        CheckDirection();
-       transform.Translate(movement);
+        //Mueve
+        Move();
+    }
+
+    private void Move()
+    {
+        //Establece el vector3 para el translate
+        Vector3 movement = Vector3.zero;
+        //Multiplica el vector 3 por la direccion, velocidad y deltatime
+        movement[(int)axis] = direction * speed * Time.deltaTime;
+        transform.Translate(movement);
     }
 
     private void CheckDirection()
     {
-        if (Mathf.Abs(transform.position[axis] - spawnpoint[axis]) > 5)
+        //Distancia que se movio la plataforma
+        float offset = transform.position[(int)axis] - spawnpoint[(int)axis];
+
+        //Si la plataforma se pasa de la distancia, cambiar la direccion
+        if (direction > 0 && offset >= distance)
         {
-            movement[axis] *= -1;
+            direction = -1;
         }
+        else if (direction < 0 && offset <= -distance)
+        {
+            direction = 1;
+        }
+        
     }
 }
